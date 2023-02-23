c0adffbfa87038d9e5c25725d8aef40a5f6b1c02
JelenaMarjanovic <jelena.marjanovic.13@gmail.com>
2023-02-23 01:28:12 +0100

    Exercise 1: Print the name variable in the h1 element

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
-// 1) Import the React and ReactDOM libraries
-import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-// 2) Get a reference to the div with ID root
-const el = document.getElementById('root');
+import App from './App';
 
-// 3) Tell React to take control of that element
+const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-// 4) Create a component
-function App() {
-  // const date = new Date();
-  // const time = date.toLocaleTimeString();
-
-  // return <h1>{time}</h1>
-  return <h1>{new Date().toLocaleTimeString()}</h1>
-}
-
-// 5) Show the component on the screen
 root.render(<App />);
