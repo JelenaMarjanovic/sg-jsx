718e388bed276a6e797e27119f44af5dda28d7b1
JelenaMarjanovic <jelena.marjanovic.13@gmail.com>
2023-02-24 23:16:38 +0100

    Exercise 2: Fix the JSX to follow all 5 rules - make sure it is valid

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
-// 1) Import the React and ReactDOM libraries
-import React from 'react';
 import ReactDOM from 'react-dom/client'
 
-// 2) Get a reference to the div with ID root
-const el = document.getElementById('root');
+import App from './App';
 
-// 3) Tell React to take control of that element
+const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-// 4) Create a component
-function App() {
-  return (
-    <div>
-      <input style={{ display: 'block', marginBottom: '1rem' }} spellCheck />
-      <textarea autoFocus={true} />
-    </div>
-  );
-}
-
-// 5) Show the component on the screen
 root.render(<App />);
