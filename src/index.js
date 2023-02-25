3ec346370366235e9a370798dfad1f1528779b34
JelenaMarjanovic <jelena.marjanovic.13@gmail.com>
2023-02-25 04:53:19 +0100

    Extracting components

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 // 1) Import the React and ReactDOM libraries
 import React from 'react';
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 
-// 2) Get a reference to the div with ID root
+// 2) Import the component
+import App from './App';
+
+// 3) Get a reference to the div with ID root
 const el = document.getElementById('root');
 
-// 3) Tell React to take control of that element
+// 4) Tell React to take control of that element
 const root = ReactDOM.createRoot(el);
-
-// 4) Create a component
-function App() {
-  return (
-    <div>
-      <input style={{ display: 'block', marginBottom: '1rem' }} spellCheck />
-      <textarea autoFocus={true} />
-    </div>
-  );
-}
 
 // 5) Show the component on the screen
 root.render(<App />);
